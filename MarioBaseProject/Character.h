d6863eeb09a35b56f637987ad63093bdc52a3d54
Harry Wheadon <91532361+HarryWheadon@users.noreply.github.com>
2022-04-28 10:20:19 +0100

    Updated Screen scrolling background

--- MarioBaseProject/Character.h
+++ MarioBaseProject/Character.h
@@ -35,7 +35,7 @@ public:
 	~Character();
 
 	float GetCollisionRadius();
-	virtual void Render();
+	virtual void Render(SDL_Rect camera_rect);
 	virtual void Update(float deltaTime, SDL_Event e);
 	void SetPosition(Vector2D new_position);
 	Vector2D GetPosition();
